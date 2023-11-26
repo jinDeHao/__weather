fe46b1c7bdc428b9376b9bbff339fd94feda6c36
omarIdhmaid <o.idhmaid@gmail.com>
2023-11-26 22:46:10 +0100

    try it

--- api/weather.py
+++ api/weather.py
@@ -2,18 +2,18 @@ from flask import Flask, Blueprint, make_response, render_template, request
 from flask_cors import CORS
 import requests
 from .units.city import getcity, getweather
-from werkzeug.middleware.proxy_fix import ProxyFix
+# from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 # Use the ProxyFix middleware to get the real client IP address
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
+# app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 
 
 @app.route('/')
 def auto():
-    client_ip = request.remote_addr
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     city = getcity(client_ip)
     weather = getweather(city)
     if weather == {}:
